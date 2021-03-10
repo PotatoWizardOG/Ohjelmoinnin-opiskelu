9638f09f3dfc0953df19403bc2ffd3ff2602035c
PotatoWizardOG <nelli.tommila@gmail.com>
2021-03-10 10:59:15 +0200

    javascript Nelli 10.3.

---
+++ javascript/jotain.js
@@ -0,0 +1,15 @@
+function heippa()
+{
+  var haettuTeksti = getElementById('teksti').value;
+  var sananPituus = haettuTeksti.length;
+  var uusiSana = '';
+  for(var x; x = sananPituus; x--)
+  {
+    uusiSana = uusiSana + haettuTeksti[x];
+
+  }
+  document.getElementById('vastaus').innerHTML = uusiSana
+
+
+
+}
